aaac013f58105cae8cdf771875096dd6d550a353
artist2026777-arch <artist2026777@gmail.com>
2026-02-06 10:01:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 http.createServer((req, res) => {
-  res.end("VPS работает");
-}).listen(3000);
+  res.end("VPS работает 24/7 🚀");
+}).listen(PORT);
